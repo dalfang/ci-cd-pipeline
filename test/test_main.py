4ead13683565260b0584f220f9fa3f106cd170f1
ddalynng <d.fangloy@gmail.com>
2025-07-24 23:31:21 +0200

    added new test function

--- test/test_main.py
+++ test/test_main.py
@@ -1,7 +1,13 @@
-from src.main import add
+from src.main import add, subtract
 
 
 def test_add():
     assert add(1,2) == 3
-    assert add(0,0) == 1
+    assert add(0,0) == 0
     assert add(5,5) == 10
+
+
+def test_subtarct_function():
+    assert subtract(5, 3) == 2
+    assert subtract(0, 0) == 0
+    assert subtract(10, 5) == 5
